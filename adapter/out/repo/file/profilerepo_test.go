369c27de41963c4b3f10e96a7c78e6703566bc5d
Maciej Bedra <bedra.maciej@gmail.com>
2021-04-19 18:47:02 +0200

    tests(#2): readings of file profile repository

---
+++ adapter/out/repo/file/profilerepo_test.go
@@ -0,0 +1,49 @@
+package file
+
+import (
+	"testing"
+)
+
+func setup() *fileProfileRepo {
+	return NewFileProfileRepo("../../../../assets/onepassword_data")
+}
+
+func TestGetIterations(t *testing.T) {
+	repo := setup()
+	expected := 50000
+	iterations := repo.GetIterations()
+
+	if iterations != expected {
+		t.Errorf("GetIterations() = %d; expected %d", iterations, expected)
+	}
+}
+
+func TestGetMasterKey(t *testing.T) {
+	repo := setup()
+	expected := "b3BkYXRhMDEAAQAAAAAAACN8JuE76yN6hbjqzEvd0RGnu3vufPcfAZ35JoyzdR1WPRvr8DMefe9MJu65DmHSwjObPC0jznXpafJQob6CNzKCNoeVC+GXIvLckvAuYUNSwILQQ1jEIcHdyQ0H2MbJ+0YlWEbvlQ8UVH5bcrMqDmTPPSRkbUG3/dV1NKHdgI0V6N/kKZ737oo+kj3ChJZQTKywvmR6RgB5et5stBaUwutNQbZ0znYtZumIlf3pjdqGK4RyCHSwmwgLUO+VFLTqDjoZ9dUcy4hQzSZiPlba3vK8vGJRlN0Qf2Y6dUj5kYAwdYdOzE/Ji3hbTNVsPOm8sjzPcPGQj8haW5UgzSDZ0mo7+ymsKJwSYjAsgvawh31WY2m5j7VR+50ERDTEyxxQ3LW7WgetAxX9l0LX0O3Jue1oW/p2l44ij9qiN9rkFScx"
+	masterKey := repo.GetMasterKey()
+
+	if masterKey != expected {
+		t.Errorf("GetMasterKey() = %v; expected %v", masterKey, expected)
+	}
+}
+
+func TestGetOverviewKey(t *testing.T) {
+	repo := setup()
+	expected := "b3BkYXRhMDFAAAAAAAAAAIy1hZwIGeiLn4mLE1R8lEwIOye95GEyfZcPKlyXkkb0IBTfCXM+aDxjD7hOliuTM/YMIqxK+firVvW3c5cp2QMgvQHpDW2AsAQpBqcgBgRUCSP+THMVg15ZeR9lI77mHBpTQ70D+bchvkSmw3hoEGot7YcnQCATbouhMXIMO52D"
+	overviewKey := repo.GetOverviewKey()
+
+	if overviewKey != expected {
+		t.Errorf("GetOverviewKey() = %v; expected %v", overviewKey, expected)
+	}
+}
+
+func TestGetSalt(t *testing.T) {
+	repo := setup()
+	expected := "P0pOMMN6Ow5wIKOOSsaSQg=="
+	salt := repo.GetSalt()
+
+	if salt != expected {
+		t.Errorf("GetSalt() = %v; expected %v", salt, expected)
+	}
+}
