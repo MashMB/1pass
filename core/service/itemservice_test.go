a74f2a3787adc6b5a5aab4b2821d72313960004b
Maciej Bedra <bedra.maciej@gmail.com>
2021-04-21 18:59:19 +0200

    tests(#3): get simple items unit test

---
+++ core/service/itemservice_test.go
@@ -0,0 +1,55 @@
+// Unit tests for default item service.
+//
+// @author TSS
+
+package service
+
+import (
+	"testing"
+
+	"github.com/mashmb/1pass/adapter/out/repo/file"
+	"github.com/mashmb/1pass/adapter/out/util/crypto"
+	"github.com/mashmb/1pass/core/domain"
+	"github.com/mashmb/1pass/port/out"
+)
+
+func setupItemAndKeyService() (*dfltItemService, KeyService) {
+	vaultPath := "../../assets/onepassword_data"
+	var crytpoUtils out.CrytpoUtils
+	var itemRepo out.ItemRepo
+	var profileRepo out.ProfileRepo
+
+	var keyService KeyService
+
+	crytpoUtils = crypto.NewPbkdf2CryptoUtils()
+	itemRepo = file.NewFileItemRepo(vaultPath)
+	profileRepo = file.NewFileProfileRepo(vaultPath)
+
+	keyService = NewDfltKeyService(crytpoUtils, profileRepo)
+
+	return NewDfltItemService(keyService, itemRepo), keyService
+}
+
+func TestGetSimple(t *testing.T) {
+	itemService, keyService := setupItemAndKeyService()
+	expected := 10
+	first := "Bank of America"
+	last := "YouTube"
+	pass := "freddy"
+	derivedKey, derivedMac, _ := keyService.DerivedKeys(pass)
+	overviewKey, overviewMac, _ := keyService.OverviewKeys(derivedKey, derivedMac)
+	keys := domain.NewKeys(derivedKey, derivedMac, nil, nil, overviewKey, overviewMac)
+	items := itemService.GetSimple(keys)
+
+	if len(items) != 10 {
+		t.Errorf("GetSimple() = %d; expected = %d", len(items), expected)
+	}
+
+	if items[0].Title != first {
+		t.Errorf("GetSimple() = %v; expected = %v", items[0].Title, first)
+	}
+
+	if items[len(items)-1].Title != last {
+		t.Errorf("GetSimple() = %v; expected = %v", items[len(items)-1], last)
+	}
+}
