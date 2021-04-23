d4af6b253534a26f966ec5f9057fb435c2ab0a4c
Maciej Bedra <bedra.maciej@gmail.com>
2021-04-23 14:04:29 +0200

    feat(#6): CLI list items command

--- cli/cobra.go
+++ cli/cobra.go
@@ -6,7 +6,6 @@ package cli
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/mashmb/1pass/port/in"
 	"github.com/spf13/cobra"
@@ -28,10 +27,20 @@ func (cli *cobraCli) init() *cobra.Command {
 		Short: "1Password Linux CLI explorer",
 		Long: `Fast and Linux user friendly application used to explore 1Password OPVault format. Check your credentials 
 efficiently in terminal.`,
+	}
+
+	listCmd := &cobra.Command{
+		Use:   "list [OPVault] [master_password]",
+		Short: "Get list of items stored in 1Passowrd OPVault format (logins only)",
+		Long: `Get list of items stored in 1Passowrd OPVault format (logins only). Items will be displayd in form of 
+[<UID>] --- <title>. UID value is required for item overview and details identifiaction.`,
+		Args: cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello World")
+			cli.cliControl.GetItems(args[0], args[1])
 		},
 	}
+
+	rootCmd.AddCommand(listCmd)
 
 	return rootCmd
 }
@@ -40,6 +49,6 @@ func (cli *cobraCli) Run() {
 	rootCmd := cli.init()
 
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
 	}
 }
