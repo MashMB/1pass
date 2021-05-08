fa62126b5237ea8eebf39212bd8b257a3f236e52
Maciej Bedra <bedra.maciej@gmail.com>
2021-05-08 21:06:17 +0200

    tests(#31): handle configurable timeout in update facade unit tests

--- 1pass-app/test/core/facade/updatefacade_test.go
+++ 1pass-app/test/core/facade/updatefacade_test.go
@@ -12,19 +12,24 @@ import (
 	corefacade "github.com/mashmb/1pass/1pass-core/core/facade"
 	"github.com/mashmb/1pass/1pass-core/core/service"
 	"github.com/mashmb/1pass/1pass-core/port/out"
+	"github.com/mashmb/1pass/1pass-parse/adapter/out/repo/file"
 	"github.com/mashmb/1pass/1pass-up/adapter/out/github"
 )
 
 func setupUpdateFacade() corefacade.UpdateFacade {
+	var configRepo out.ConfigRepo
 	var updater out.Updater
 
+	var configService service.ConfigService
 	var updateService service.UpdateService
 
+	configRepo = file.NewFileConfigRepo("../../../../assets/1pass.yml")
 	updater = github.NewGithubUpdater()
 
+	configService = service.NewDfltConfigService(configRepo)
 	updateService = service.NewDfltUpdateService(updater)
 
-	return corefacade.NewDfltUpdateFacade(updateService)
+	return corefacade.NewDfltUpdateFacade(configService, updateService)
 }
 
 func isOnline() bool {
