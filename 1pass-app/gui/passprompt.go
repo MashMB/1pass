4beca23674ff831ab916fe984e66e55e1bcb4a9e
Maciej Bedra <bedra.maciej@gmail.com>
2021-05-11 15:49:09 +0200

    feat(#34): password prompt widget

---
+++ 1pass-app/gui/passprompt.go
@@ -0,0 +1,57 @@
+// GUI password prompt.
+//
+// @author TSS
+
+package gui
+
+import "github.com/jroimartin/gocui"
+
+type passwordPrompt struct {
+	name    string
+	title   string
+	handler func(ui *gocui.Gui, view *gocui.View) error
+}
+
+func newPasswordPrompt(handler func(ui *gocui.Gui, view *gocui.View) error) *passwordPrompt {
+	return &passwordPrompt{
+		name:    "passPrompt",
+		title:   "Password",
+		handler: handler,
+	}
+}
+
+func (pp *passwordPrompt) abort(_ *gocui.Gui, _ *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func (pp *passwordPrompt) Keybindings(ui *gocui.Gui) error {
+	if err := ui.SetKeybinding(pp.name, gocui.KeyEnter, gocui.ModNone, pp.handler); err != nil {
+		return err
+	}
+
+	if err := ui.SetKeybinding(pp.name, gocui.KeyCtrlQ, gocui.ModNone, pp.abort); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pp *passwordPrompt) Layout(ui *gocui.Gui) error {
+	maxX, maxY := ui.Size()
+
+	if view, err := ui.SetView(pp.name, maxX/2-30, maxY/2-1, maxX/2+30, maxY/2+1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		view.Title = pp.title
+		view.Editable = true
+		view.Mask = '*'
+
+		if _, err := ui.SetCurrentView(pp.name); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
