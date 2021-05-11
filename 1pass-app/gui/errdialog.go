149814c0fdc07efc02639001a985654e46e1e851
Maciej Bedra <bedra.maciej@gmail.com>
2021-05-11 17:23:30 +0200

    feat(#34): error dialog

---
+++ 1pass-app/gui/errdialog.go
@@ -0,0 +1,56 @@
+// GUI error dialog.
+//
+// @author TSS
+
+package gui
+
+import (
+	"fmt"
+
+	"github.com/jroimartin/gocui"
+)
+
+type errorDialog struct {
+	name  string
+	title string
+	err   error
+}
+
+func newErrorDialog() *errorDialog {
+	return &errorDialog{
+		name:  "errDialog",
+		title: "Error",
+	}
+}
+
+func (ed *errorDialog) panicQuit(_ *gocui.Gui, _ *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func (ed *errorDialog) Keybindings(ui *gocui.Gui) error {
+	if err := ui.SetKeybinding(ed.name, gocui.KeyEnter, gocui.ModNone, ed.panicQuit); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ed *errorDialog) Layout(ui *gocui.Gui) error {
+	maxX, maxY := ui.Size()
+
+	if view, err := ui.SetView(ed.name, maxX/2-30, maxY/2-1, maxX/2+30, maxY/2+1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		view.Title = ed.title
+
+		if _, err := ui.SetCurrentView(ed.name); err != nil {
+			return err
+		}
+
+		fmt.Fprint(view, ed.err)
+	}
+
+	return nil
+}
